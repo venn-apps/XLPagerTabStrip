e2347e10e734e91f327a55bdeea9124ee48d077e
Josh Gare <josh@jgapplications.com>
2021-08-03 21:31:44 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,13 +6,18 @@ let package = Package(
   name: "XLPagerTabStrip",
   platforms: [.iOS(.v9)],
   products: [
-    .library(name: "XLPagerTabStrip", targets: ["XLPagerTabStrip"])
+    .library(name: "XLPagerTabStrip", targets: ["XLPagerTabS.gittrip"])
   ],
   dependencies: [],
   targets: [
     .target(
       name: "XLPagerTabStrip",
-      dependencies: ["FXPageControl"],
+      dependencies: [
+        .package(
+          url: "https://github.com/venn-apps/FXPageControl.git",
+          .branch("master")
+        )
+      ],
       path: "Sources",
       resources: [.process("Sources/ButtonCell.xib")],
       publicHeadersPath: "XLPagerTabStrip"),
